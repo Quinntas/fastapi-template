f72dc283a52460e381676782241a13dadb483511
quintas <caioquintassantiago@gmail.com>
2025-02-12 10:38:53 -0300

    small fixes

--- src/shared/resources/health_check/health_check_controller.py
+++ src/shared/resources/health_check/health_check_controller.py
@@ -6,7 +6,9 @@ from src.core.responses import MessageResponse
 
 class HealthCheckController(Controller, ABC):
     def __init__(self):
-        super().__init__()
+        super().__init__(
+            tags=['Utils']
+        )
 
     async def handle(self) -> MessageResponse:
         return MessageResponse(message="ok")
--- src/start/routes.py
+++ src/start/routes.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 from src.shared.infra.shared_router import shared_router
 
-v1_router = APIRouter(prefix='/v1')
+v1_router = APIRouter(prefix='/api/v1')
 
 v1_router.include_router(shared_router)
